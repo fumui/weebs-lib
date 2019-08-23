714b0eb49461501cebc23172a3f842d26ebf1645
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-08-23 21:24:14 +0700

    relocating bootstrap cdn and changing auth method

--- src/App.js
+++ src/App.js
@@ -9,28 +9,21 @@ import BookDetail from './Pages/BookDetail';
 class App extends React.Component {
   constructor(props){
     super(props)
+    const isLoggedIn = document.cookie.includes("token=Bearer ")
     this.state = {
-      loggedIn: false
+      loggedIn: isLoggedIn
     }
-    const isLoggedIn = document.cookie.includes("token")
-    this.setState({loggedIn:isLoggedIn})
   }
 
   componentDidMount(){
-    const isLoggedIn = document.cookie.includes("token")
-    if(this.state.loggedIn !== isLoggedIn){
-      this.setState({loggedIn:isLoggedIn})
-    }
+    // const isLoggedIn = document.cookie.includes("token=Bearer ")
+    // if(this.state.loggedIn !== isLoggedIn){
+    //   this.setState({loggedIn:isLoggedIn})
+    // }
   }
   render(){
     return (
       <div className='App'>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
         <Router>
         <Route
             exact={true}
